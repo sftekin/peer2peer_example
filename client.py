651823654d02a6113bcb5d50ae2f25e1d8aef81d
sftekin <sftekin25@gmail.com>
2023-02-10 21:53:21 -0500

    init commit

--- client.py
+++ client.py
@@ -1,20 +1,27 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+
 from random import randint
 
 
 class Client(DatagramProtocol):
-    def __init__(self, host, port):
-        if host == "localhost":
-            host = "127.0.0.1"
-        self.id = host, port
-        self.address = None
-        self.server = "127.0.0.1", 9999
+    def __init__(self, host="localhost", port="8000", capacity=1):
+        self.client_add = "127.0.0.1" if host == "localhost" else host
+        self.client_port = port
+        self.capacity = capacity  # the number of peers that this client can connect
 
-        print("Working on id:", self.id)
+        # server info
+        self.server_add = "127.0.0.1"
+        self.server_port = 9999
+
+        # peers info
+        self.peers_info = {}
+
+        print(f"Client {self.client_add}/{self.client_port} is created.")
 
     def startProtocol(self):
-        self.transport.write("ready".encode("utf-8"), self.server)
+        server_message = f"ready-{self.capacity}".encode("utf-8")
+        self.transport.write(server_message, self.server)
 
     def datagramReceived(self, datagram: bytes, addr):
         datagram = datagram.decode("utf-8")
@@ -23,14 +30,18 @@ class Client(DatagramProtocol):
             self.address = input("Write host: "), int(input("Write port: "))
             reactor.callInThread(self.send_message)
         else:
+            # perform
             print(addr, ":", datagram)
 
     def send_message(self):
         while True:
             self.transport.write(input(":::").encode("utf-8"), self.address)
 
+    def parse_server_message(self, datagram):
+        # the server will tell you where you can connect
+        pass
 
 if __name__ == '__main__':
-    port = randint(1000, 5000)
-    reactor.listenUDP(port, Client("localhost", port))
+    r_port = randint(1000, 5000)
+    reactor.listenUDP(r_port, Client("localhost", r_port))
     reactor.run()
--- server.py
+++ server.py
@@ -1,17 +1,52 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+import numpy as np
 
 
 class Server(DatagramProtocol):
     def __init__(self):
-        self.clients = set()
+        self.clients2cap = {}
+        self.clients2addr = {}
+        self.adjacency_mat = None
+        self.n_nodes = 0
 
     def datagramReceived(self, datagram: bytes, addr):
         datagram = datagram.decode("utf-8")
-        if datagram == "ready":
+        if "ready" not in datagram:
+            capacity = datagram.split("-")[1]
+            if addr not in self.clients2addr.keys():
+                self.add_node(addr=addr, capacity=capacity)
+                print(f"ADD: Client {addr} added with capacity {capacity}")
+            else:
+                raise Warning("This node already exists")
+
+            # send the neighbouring info
+
             adds = "\n".join([str(x) for x in self.clients])
             self.transport.write(adds.encode("utf-8"), addr)
-            self.clients.add(addr)
+            self.clients.append(addr)
+        else:
+            raise Warning("Server only accept registration requests!")
+
+    def add_node(self, addr, capacity):
+        if self.adjacency_mat is None:
+            self.adjacency_mat = np.array([[1]])
+            self.n_nodes = 1
+            self.clients2cap[0] = capacity
+            self.clients2addr[0] = addr
+        else:
+            # update adjacent mat
+            temp_mat = np.zeros((self.n_nodes + 1, self.n_nodes + 1))
+            temp_mat[:self.n_nodes, :self.n_nodes] = self.adjacency_mat.copy()
+            temp_mat[-1, -1] = 1  # set the self adjacent
+            random_ngh = np.random.permutation(range(self.n_nodes))[:capacity]
+            temp_mat[random_ngh] = 1  # set the neighbours randomly
+            self.adjacency_mat = temp_mat
+
+            # update the info
+            self.clients2cap[self.n_nodes] = capacity
+            self.clients2addr[self.n_nodes] = addr
+            self.n_nodes += 1
 
 
 if __name__ == '__main__':
